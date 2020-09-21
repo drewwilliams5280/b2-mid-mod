d9fed9a85e540d19a22949a1181f0596b6dd7f97
Drew Williams <drewwilliams5280@gmail.com>
2020-09-21 09:54:00 -0600

    Adds names to flights on index page, completes US1

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -10,4 +10,25 @@ RSpec.describe "flights index page", type: :feature do
     expect(page).to have_content(flight_1.number)
     expect(page).to have_content(flight_2.number)
   end
+
+  it "can show flight passengers" do
+    airline_1 = Airline.create!(name: "Delta Airlines")
+    flight_1 = Flight.create!(number: Faker::Number.between(from: 1000, to: 99999), airline: airline_1)
+    flight_2 = Flight.create!(number: Faker::Number.between(from: 1000, to: 99999), airline: airline_1)
+    passenger_1 = Passenger.create!(name: "#{Faker::Name.first_name} #{Faker::Name.last_name}", age: Faker::Number.between(from: 1, to: 90))
+    passenger_2 = Passenger.create!(name: "#{Faker::Name.first_name} #{Faker::Name.last_name}", age: Faker::Number.between(from: 1, to: 90))
+    passenger_3 = Passenger.create!(name: "#{Faker::Name.first_name} #{Faker::Name.last_name}", age: Faker::Number.between(from: 1, to: 90))
+    flight_1.passengers << passenger_1
+    flight_2.passengers << passenger_2
+    flight_2.passengers << passenger_3
+    visit "/flights"
+    save_and_open_page
+    within("#flight-#{flight_1.number}") do
+      expect(page).to have_content(passenger_1.name)
+    end
+    within("#flight-#{flight_2.number}") do
+      expect(page).to have_content(passenger_2.name)
+      expect(page).to have_content(passenger_3.name)
+    end
+  end
 end
